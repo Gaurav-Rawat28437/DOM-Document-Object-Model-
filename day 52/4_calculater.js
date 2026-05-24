48e4a85efad50d86acaf3b35537daf3d29c542ef
Gaurav Singh Rawat <gr28437@gmail.com>
2026-05-24 16:40:37 +0530

    commit

--- day 52/4_calculater.js
+++ day 52/4_calculater.js
@@ -1,17 +1,28 @@
-const but=document.getElementsByTagName("button")
-const output=document.getElementsByTagName("h2")
+const but=document.getElementsByTagName("button")[0]
+const output=document.getElementsByTagName("h2")[0]
 
 
 but.addEventListener("click",()=>{
 
     const n1=Number(prompt("enter number 1"))
     const n2=Number(prompt("enter number 2"))
-    const operation=Number(prompt("enter the operation( +  -  *  / )"))
+    const operation=prompt("enter the operation( +  -  *  / )")
 
 
     if (!isNaN(n1) && !isNaN(n2) && ["+","-","*","/"].includes(operation))
     {
-
+        switch(operation){
+            case "+":output.innerText=n1+n2
+                        break
+            case "-":output.innerText=n1-n2
+                        break
+            case "*":output.innerText=n1*n2
+                        break
+            case "/":output.innerText=n1/n2
+                        break
+            default:output.innerText="invalid outpuy"
+                    
+        }
     }
     else
     {
