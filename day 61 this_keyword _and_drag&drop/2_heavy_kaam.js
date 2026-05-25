d9e00a0f8b083c0d79f8038db4b68eaa57627ba6
Gaurav Singh Rawat <gr28437@gmail.com>
2026-05-25 09:25:33 +0530

    commit

--- day 61 this_keyword _and_drag&drop/2_heavy_kaam.js
+++ day 61 this_keyword _and_drag&drop/2_heavy_kaam.js
@@ -7,3 +7,42 @@ myBtn.addEventListener("click",(e)=>{
         console.log("heavly kaaam")
     }
 })
+
+
+// Meaning:
+// e.isTrusted → checks if event is real or fake
+// 1. Real user event
+
+// When user clicks button:
+
+// button.addEventListener("click", (e) => {
+//     console.log(e.isTrusted)
+// })
+
+// Output:
+
+// true
+
+// Because:
+
+// real mouse click
+// real keyboard action
+// 2. Fake (programmatic) event
+
+// If event is triggered using JS:
+
+// myBtn.click()
+
+// Then:
+
+// e.isTrusted = false
+// Your code meaning
+// if(e.isTrusted)
+
+// 👉 run code only if user actually clicked
+
+// So:
+
+// console.log("heavly kaaam")
+
+// runs only for real user action.
